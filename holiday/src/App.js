c6dadde9a300e64b18c2a54e2d81175451a58cee
WakudeM <vakudemukesh@gmail.com>
2023-01-11 16:27:17 +0530

    Define All roots of packages

--- holiday/src/App.js
+++ holiday/src/App.js
@@ -12,18 +12,32 @@ import DynamicPackage from './thomascook/DynamicPackage';
 import ErrorPage from './thomascook/ErrorPage';
 import HplusPackage from './thomascook/HplusPackage';
 import SrpPage from './thomascook/SrpPage';
+import PdpPageHplus from './thomascook/PdpPageHplus';
+import 
+{PATH_ROOT,INDIA_TOUR_PACKAGES,INTERNATIONAL_TOUR_PACKAGES
+  ,INDIA_TOUR_PACKAGES_WITH_TOUR,INTERNATIONAL_TOUR_PACKAGES_WITH_TOUR,
+  INTERNATIONAL_TOUR_PACKAGES_WITH_TOUR_AND_BUDGET_MONTH,INDIA_TOUR_PACKAGES_WITH_TOUR_AND_BUDGET_MONTH,
+  DYNA_PDP ,HPLUS_PDP,HPLUS_PDP_INTERNATIONAL, HPLUS_PDP_INDIA
+} from './thomascook/Constants/RouteConstants';
 function App() {
+  console.log(INTERNATIONAL_TOUR_PACKAGES_WITH_TOUR_AND_BUDGET_MONTH ,"value")
   return (
     <>
       <Router>
         <div className="main-wrapper">
 
           <Routes>
-            <Route exact path="/holiday" element={<TcHoliday />} />
-            <Route exact path="/holiday/packages/customized/:packagename/pkgId/:packageid" element={<DynamicPackage />} />
-            <Route exact path="/holiday/international-tour-packages/:packagename/holidayBudget/:budget/holidayMonth/:month" element={<SrpPage />} />
-            <Route exact path="/holiday/packages/:hpluspackage/pkgId/:hpluspkgid/destination/:pkgidandpkgname" element={<HplusPackage />} />
-           
+            <Route exact path={PATH_ROOT} element={<TcHoliday />} />
+            <Route exact path={INDIA_TOUR_PACKAGES} element={<TcHoliday />} />
+            <Route exact path={INTERNATIONAL_TOUR_PACKAGES} element={<TcHoliday />} />
+            <Route exact path={INDIA_TOUR_PACKAGES_WITH_TOUR} element={<SrpPage />} />
+            <Route exact path={INTERNATIONAL_TOUR_PACKAGES_WITH_TOUR} element={<SrpPage />} />
+            <Route exact path={INTERNATIONAL_TOUR_PACKAGES_WITH_TOUR_AND_BUDGET_MONTH} element={<SrpPage />} />
+            <Route exact path={INDIA_TOUR_PACKAGES_WITH_TOUR_AND_BUDGET_MONTH} element={<SrpPage />} />
+            <Route exact path={DYNA_PDP} element={<DynamicPackage />} /> 
+            <Route exact path={HPLUS_PDP} element={<HplusPackage />} />
+            <Route exact path={HPLUS_PDP_INTERNATIONAL} element={<PdpPageHplus />} />    
+            <Route exact path={HPLUS_PDP_INDIA} element={<PdpPageHplus />} />           
             <Route path="*" element={<ErrorPage />} />
           </Routes>
 
---
+++ holiday/src/thomascook/PdpPageHplus.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PdpPageHplus = () => {
+    return (
+        <div>
+            <h1>Hplus PDP Page</h1>
+        </div>
+    );
+}
+
+export default PdpPageHplus;
